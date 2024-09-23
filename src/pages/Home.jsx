ddecc34e2d63cfc4b436b2636cb86d841638d447
dhameliyahit <heetdhameliya59@gmail.com>
2024-09-23 16:33:30 +0530

    :fix again header path in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React,{useState,useEffect} from 'react'
-import Header from '../components/header'
+import Header from '../components/Header.jsx'
 import axios from 'axios';
 
 const Home = () => {
