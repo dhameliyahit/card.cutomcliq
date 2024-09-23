5906ecead5f4161feb196cbcc60eeea9f6078aed
dhameliyahit <heetdhameliya59@gmail.com>
2024-09-23 16:30:19 +0530

    fix:header patch

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from '../components/header'
+import Header from '../components/Header.jsx'
 import SocialLinksForm from '../components/SocialLinkForm'
 
 const Dashboard = () => {
